6f09b76923fd468447a1ab281b5d372d90677679
Marcos Botene <marcosjbotene@gmail.com>
2023-09-03 15:20:45 -0300

    feat: finished view image component

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,31 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+
+      <ModalContent maxWidth='900px' maxHeight='600px'>
+        <ModalBody p={0}>
+          <Image
+            src={imgUrl}
+            maxWidth='900px'
+            width='full'
+            maxHeight='600px'
+            height='full'
+          />
+        </ModalBody>
+
+        <ModalFooter
+          justifyContent='flex-start'
+          rounded='0 0 6px 6px'
+          bgColor='pGray.800'
+        >
+          <Link href={imgUrl} target='_blank' fontSize='sm' color='pGray.50'>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
