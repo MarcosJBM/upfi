a1eeb3773e9144a0cabe402b0627d3cc5fded0bb
Marcos Botene <marcosjbotene@gmail.com>
2023-09-03 15:16:16 -0300

    feat: finished home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
+import { Fragment, useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
@@ -7,6 +7,31 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+
+interface FetchImagesProps {
+  pageParam?: string | null;
+}
+
+interface ImageProps {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface FetchImagesResponse {
+  data: ImageProps[];
+  after: string;
+}
+
+async function fetchImages({ pageParam = null }: FetchImagesProps) {
+  const { data } = await api.get<FetchImagesResponse>('api/images', {
+    params: { after: pageParam },
+  });
+
+  return data;
+}
 
 export default function Home(): JSX.Element {
   const {
@@ -16,28 +41,35 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images'
-    // TODO AXIOS REQUEST WITH PARAM
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery({
+    queryKey: ['images'],
+    queryFn: fetchImages,
+    getNextPageParam: lastPage => lastPage.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    if (!data) return [];
+
+    return data.pages.map(page => page.data).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
-    <>
+    <Fragment>
       <Header />
 
       <Box maxW={1120} px={20} mx='auto' my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} mt='10'>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
-    </>
+    </Fragment>
   );
 }
